74c26a03e83378aab1829bc9e3c616a10b0bc57c
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2024-10-23 15:35:19 +0530

    file modified

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 
 
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://password789:password789@password-reset-cluster.wxa7m.mongodb.net/password-reset-cluster');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
